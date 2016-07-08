3f77c678b788878c6d9abdaa2be248d198f02fea
Michael Mitchell <micmitch44@gmail.com>
2016-07-08 11:44:43 -0700

    Work on DCinv.py script.

--- SciPy2016/notebooks/DC/CleanVersions/TKCExample_DCinv.py
+++ SciPy2016/notebooks/DC/CleanVersions/TKCExample_DCinv.py
@@ -1,6 +1,7 @@
 from SimPEG import DataMisfit, Regularization, Optimization, Directives, InvProblem, Inversion
+import pickle
 
-
+# Run survey setup and forward modelling
 run TKCExample_DCfwd.py
 
 
@@ -8,24 +9,28 @@ run TKCExample_DCfwd.py
 depth = 1./(abs(mesh.gridCC[:,2]-zc))**1.5
 depth = depth/depth.max()
 
+# Setup inversion object
 regmap = Maps.IdentityMap(nP=m0.size)
+# Assign uncertainties
 std = 0.05
 eps = 1e-3
 survey.std = std
 survey.eps = eps
-#TODO put warning when dobs is not set!
 survey.dobs = dobs
+# Define datamisfit portion of objective function
 dmisfit = DataMisfit.l2_DataMisfit(survey)
+# Define regulatization (model objective function)
 reg = Regularization.Simple(mesh, mapping=regmap, indActive=~airind)
 reg.wght = depth[~airind]
 opt = Optimization.InexactGaussNewton(maxIter = 20)
 invProb = InvProblem.BaseInvProblem(dmisfit, reg, opt)
-# Create an inversion object
+# Define inversion parameters
 beta = Directives.BetaSchedule(coolingFactor=5, coolingRate=2)
 betaest = Directives.BetaEstimate_ByEig(beta0_ratio=1e0)
 save = Directives.SaveOutputEveryIteration()
+savemodel = Directives.SaveModelEveryIteration()
 target = Directives.TargetMisfit()
-inv = Inversion.BaseInversion(invProb, directiveList=[beta,betaest, save, target])
+inv = Inversion.BaseInversion(invProb, directiveList=[beta,betaest, save, target, savemodel])
 reg.alpha_s = 1e-1
 reg.alpha_x = 1.
 reg.alpha_y = 1.
@@ -33,8 +38,21 @@ reg.alpha_z = 1.
 problem.counter = opt.counter = Utils.Counter()
 opt.LSshorten = 0.5
 opt.remember('xc')
+
+# Run inversion
 mopt = inv.run(m0)
+
+# Apply mapping to model to get and save recovered conductivity
 sigopt = mapping*mopt
 np.save("sigest_singlesrc_withdweights", sigopt)
+
+# Calculate dpred
+dpred = survey.dpred(np.log(sigopt_withdepth[~airind]))
+
+# Pickle results for easy access
+Results = {"model_true":sigma, "model_pred":sigopt, "Obs":dobs, "Pred":dpred}
+outputs = open("DCresults", 'wb')
+pickle.dump(Results, outputs)
+outputs.close()
 
 
