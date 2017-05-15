7abaa0753a68332ffdda7237ca3c5f6174e1b6d3
seogi_macbook <sgkang09@github.com>
2017-05-15 15:50:11 -0700

    regenerate TKCATEMexample.p

--- SciPy2016/notebooks/ATEM/TKCATEMfwd.py
+++ SciPy2016/notebooks/ATEM/TKCATEMfwd.py
@@ -1,6 +1,7 @@
-from SimPEG import EM, Mesh, Utils, np
+from SimPEG import EM, Mesh, Utils
+import numpy as np
 from scipy.constants import mu_0
-from pymatsolver import MumpsSolver
+from pymatsolver import PardisoSolver
 import cPickle as pickle
 
 TKCATEMexample = pickle.load( open( "../TKCATEMexample.p", "rb" ) )
@@ -8,22 +9,20 @@ TKCATEMexample = pickle.load( open( "../TKCATEMexample.p", "rb" ) )
 mesh = TKCATEMexample["mesh"]
 sigma = TKCATEMexample["sigma"]
 xyz = TKCATEMexample["xyz"]
-times = TKCATEMexample["times"]
-timeSteps = TKCATEMexample["timeSteps"]
-ntx = TKCATEMexample["ntx"]
+ntx = xyz.shape[0]
 
 # TDEM Survey
 srcLists = []
 times = np.logspace(-4, np.log10(2e-3), 10)
 for itx in range(ntx):
     rx = EM.TDEM.Rx(xyz[itx,:].reshape([1,-1]), times, 'bz')
-    src = EM.TDEM.Src.CircularLoop([rx], waveform=EM.TDEM.Src.StepOffWaveform(), loc=xyz[itx,:].reshape([1,-1]), radius = 13.) # same src location as FDEM problem
+    src = EM.TDEM.Src.CircularLoop([rx], waveform=EM.TDEM.Src.StepOffWaveform(), loc=xyz[itx, :].reshape([1, -1]), radius = 13.) # same src location as FDEM problem
     srcLists.append(src)
 
 # TDEM Problem
 survey = EM.TDEM.Survey(srcLists)
 problem = EM.TDEM.Problem_b(mesh, verbose=True)
-timeSteps = [(1e-5, 5), (1e-4, 10),(5e-4, 10)]
+timeSteps = [(1e-5, 5), (1e-4, 10), (5e-4, 10)]
 problem.timeSteps = timeSteps
 problem.pair(survey)
 problem.Solver = MumpsSolver
